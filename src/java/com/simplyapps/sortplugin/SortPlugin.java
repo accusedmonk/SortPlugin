e6de42208d460d35f3880f045caf7e9c0ce63c7f
Casey Stangel <accusedmonk@gmail.com>
2020-09-24 11:59:53 -0400

    initial commit

---
+++ src/java/com/simplyapps/sortplugin/SortPlugin.java
@@ -0,0 +1,17 @@
+package java.com.simplyapps.sortplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SortPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Suck it");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Unsuck it");
+    }
+}
